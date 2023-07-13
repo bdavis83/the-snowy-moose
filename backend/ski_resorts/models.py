85df82019caf6789f1c4befe8a44811052d18a12
bdavis83 <bdavis83@gmail.com>
2023-07-12 20:28:46 -0400

    updated models.py for ski_resorts app

--- backend/ski_resorts/models.py
+++ backend/ski_resorts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class SkiResort (models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4)
+    longitude = models.DecimalField(max_digits=8, decimal_places=4)
+    pass_type = models.CharField(max_length=100)
+    lift_ticket_price = models.DecimalField(max_digits=5, decimal_places=2)
+    peak_altitude = models.DecimalField(max_digits=5, decimal_places=0)
+    base_altitude = models.DecimalField(max_digits=5, decimal_places=0)
+    average_snowfall = models.DecimalField(max_digits=4)
+    skiiable_acreage = models.DecimalField(max_digits=5)
+    chairlifts = models.DecimalField(max_digits=3)
+    tram = models.DecimalField(max_digits=1)
+    trails = models.DecimalField(max_digits=4)
+    
+
