7edac08094dead853dc272c9aaa9f48e39e710f6
bdavis83 <bdavis83@gmail.com>
2023-07-23 22:05:27 -0400

    added fields to skiresort serializer.py

--- backend/ski_resorts/serializer.py
+++ backend/ski_resorts/serializer.py
@@ -4,5 +4,5 @@ from models import SkiResort
 class SkiResortSerializer(serializers.ModelSerializer):
     class Meta:
         model = SkiResort
-        fields = ['id', 'name', 'city', 'state', 'region', 'latitude', 'longitude', 'pass_type', 'lift_ticket_price', 'peak_altitude', 'base_altitude', 'average_snowfall', 'skiiable_acreage', 'chairlifts', 'tram', 'trails']
+        fields = ['id', 'name', 'city', 'state', 'region', 'latitude', 'longitude', 'pass_type', 'lift_ticket_price', 'peak_altitude', 'base_altitude', 'average_snowfall', 'skiiable_acreage', 'chairlifts', 'tram', 'trails', 'country', 'continent']
         depth = 1
